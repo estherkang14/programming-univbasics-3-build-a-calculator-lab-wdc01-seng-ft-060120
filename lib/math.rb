65ebcc67f6eade9051495a98164866bc4524da17
Esther Kang <estherkang14@gmail.com>
2020-05-16 22:36:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,17 +3,17 @@ def addition(num1, num2)
   puts result
   num1 + num2
 end
-addition(5, 4)
 
 def subtraction(num1, num2)
   result = num1 - num2
   puts result
   num1 - num2
 end
- 
 
 def division(num1, num2)
-
+  result = num1 / num2
+  puts result
+  num1 / num2
 end
 
 def multiplication(num1, num2)
